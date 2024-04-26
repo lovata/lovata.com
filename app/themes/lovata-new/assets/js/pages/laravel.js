cfcb2e49fac5381b0eb5a165d8acfa6bcda039ac
antalikavasilkova <antalikavasilkova@gmail.com>
2024-04-26 18:45:31 +0300

    fix after merge

--- app/themes/lovata-new/assets/js/pages/laravel.js
+++ app/themes/lovata-new/assets/js/pages/laravel.js
@@ -1,3 +1,4 @@
 import '../../../partials/components/cases/cases'
 import '../../../partials/components/services/services'
 import '../../../partials/components/portfolio/portfolio'
+import '../../../partials/extends/accordion/accordion'
